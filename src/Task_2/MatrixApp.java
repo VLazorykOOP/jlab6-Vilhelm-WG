c12f43ff521ea4420af327fa2c1e8d0e8bbed220
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-11-12 21:40:37 +0200

    work commit

--- src/Task_2/MatrixApp.java
+++ src/Task_2/MatrixApp.java
@@ -183,7 +183,21 @@ public class MatrixApp extends JFrame {
     }
 
     // --- Точка входу в програму ---
+    // --- Точка входу в програму ---
     public static void main(String[] args) {
+
+        // Альтернатива: Встановлюємо вбудований Nimbus L&F
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Якщо Nimbus недоступний, нічого страшного, буде стандартна тема
+        }
+
         // Запуск GUI в потоці обробки подій (Event Dispatch Thread)
         SwingUtilities.invokeLater(new Runnable() {
             @Override
